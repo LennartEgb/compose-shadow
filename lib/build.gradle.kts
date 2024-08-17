902cbeb62acd5ac0ca829d1d7049bc024de79488
LennartEgb <26793300+LennartEgb@users.noreply.github.com>
2024-08-17 19:09:28 +1000

    remove resources for android library

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -40,10 +40,6 @@ android {
     namespace = "dev.lennartegb.shadows"
     compileSdk = libs.versions.android.compileSdk.get().toInt()
 
-    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
-    sourceSets["main"].res.srcDirs("src/androidMain/res")
-    sourceSets["main"].resources.srcDirs("src/commonMain/resources")
-
     defaultConfig {
         minSdk = libs.versions.android.minSdk.get().toInt()
     }
