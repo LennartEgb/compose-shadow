f4e39a72c45e6ad9e6bebcfb3d42ee6444e9fdc0
LennartEgb <26793300+LennartEgb@users.noreply.github.com>
2024-08-18 10:48:47 +1000

    add binary-compatibility-validator

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.jetbrainsCompose)
     alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.binaryCompatibility)
 }
 
 kotlin {
