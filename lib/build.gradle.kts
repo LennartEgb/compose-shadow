80904da58ee863c681510243169968ca4db3578b
LennartEgb <26793300+LennartEgb@users.noreply.github.com>
2024-11-01 21:24:44 +1100

    remove buildTypes release config

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -55,11 +55,6 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-        }
-    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
