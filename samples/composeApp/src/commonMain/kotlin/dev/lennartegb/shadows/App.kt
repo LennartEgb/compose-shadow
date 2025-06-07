ca700592f7dcd15786dbbfd9cff610850f45aca6
LennartEgb <26793300+LennartEgb@users.noreply.github.com>
2025-06-07 19:43:24 +1000

    Add wasm sample

--- samples/composeApp/src/commonMain/kotlin/dev/lennartegb/shadows/App.kt
+++ samples/composeApp/src/commonMain/kotlin/dev/lennartegb/shadows/App.kt
@@ -1,7 +1,10 @@
 package dev.lennartegb.shadows
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import org.jetbrains.compose.ui.tooling.preview.Preview
@@ -9,7 +12,11 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
+    MaterialTheme(
+        colorScheme = if (isSystemInDarkTheme()) darkColorScheme() else lightColorScheme(),
+        shapes = MaterialTheme.shapes,
+        typography = MaterialTheme.typography,
+    ) {
         DetailScreen(modifier = Modifier.fillMaxSize())
     }
 }
