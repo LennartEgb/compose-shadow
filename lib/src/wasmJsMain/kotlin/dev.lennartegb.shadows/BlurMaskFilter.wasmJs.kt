6d48871ade6246daf9816b4416328398f6a9b1b6
LennartEgb <26793300+LennartEgb@users.noreply.github.com>
2025-06-07 19:29:04 +1000

    Add wasm support

---
+++ lib/src/wasmJsMain/kotlin/dev.lennartegb.shadows/BlurMaskFilter.wasmJs.kt
@@ -0,0 +1,25 @@
+package dev.lennartegb.shadows
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.asComposePaint
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.Dp
+import org.jetbrains.skia.FilterBlurMode
+import org.jetbrains.skia.MaskFilter
+
+public actual fun createBlurPaint(
+    density: Density,
+    blurRadius: Dp,
+    color: Color,
+): Paint {
+    return Paint().asFrameworkPaint().apply {
+        if (blurRadius.value > 0f) {
+            val mode = FilterBlurMode.NORMAL
+            val sigma = with(density) { blurRadius.toPx() / 2 }
+            this.maskFilter = MaskFilter.makeBlur(mode = mode, sigma = sigma)
+        }
+        this.color = color.toArgb()
+    }.asComposePaint()
+}
