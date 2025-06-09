67e2137f90c14a0643294a6a2e202ee1a455515d
LennartEgb <26793300+LennartEgb@users.noreply.github.com>
2025-06-09 12:13:27 +1000

    Animate colors on theme change

--- samples/composeApp/src/commonMain/kotlin/dev/lennartegb/shadows/App.kt
+++ samples/composeApp/src/commonMain/kotlin/dev/lennartegb/shadows/App.kt
@@ -1,22 +1,75 @@
 package dev.lennartegb.shadows
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.AnimationSpec
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
     val state = rememberAppState()
+    val palette = if (state.isDarkTheme) darkColorScheme() else lightColorScheme()
+
     MaterialTheme(
-        colorScheme = if (state.isDarkTheme) darkColorScheme() else lightColorScheme(),
+        colorScheme = palette.switch(),
         shapes = MaterialTheme.shapes,
         typography = MaterialTheme.typography,
     ) {
         DetailScreen(modifier = Modifier.fillMaxSize(), appState = state)
     }
 }
+
+@Composable
+private fun animateColor(
+    targetValue: Color,
+    animationSpec: AnimationSpec<Color> = tween(durationMillis = 500),
+) = animateColorAsState(targetValue, animationSpec = animationSpec).value
+
+@Composable
+private fun ColorScheme.switch() = copy(
+    primary = animateColor(primary),
+    onPrimary = animateColor(onPrimary),
+    primaryContainer = animateColor(primaryContainer),
+    onPrimaryContainer = animateColor(onPrimaryContainer),
+    inversePrimary = animateColor(inversePrimary),
+    secondary = animateColor(secondary),
+    onSecondary = animateColor(onSecondary),
+    secondaryContainer = animateColor(secondaryContainer),
+    onSecondaryContainer = animateColor(onSecondaryContainer),
+    tertiary = animateColor(tertiary),
+    onTertiary = animateColor(onTertiary),
+    tertiaryContainer = animateColor(tertiaryContainer),
+    onTertiaryContainer = animateColor(onTertiaryContainer),
+    background = animateColor(background),
+    onBackground = animateColor(onBackground),
+    surface = animateColor(surface),
+    onSurface = animateColor(onSurface),
+    surfaceVariant = animateColor(surfaceVariant),
+    onSurfaceVariant = animateColor(onSurfaceVariant),
+    surfaceTint = animateColor(surfaceTint),
+    inverseSurface = animateColor(inverseSurface),
+    inverseOnSurface = animateColor(inverseOnSurface),
+    error = animateColor(error),
+    onError = animateColor(onError),
+    errorContainer = animateColor(errorContainer),
+    onErrorContainer = animateColor(onErrorContainer),
+    outline = animateColor(outline),
+    outlineVariant = animateColor(outlineVariant),
+    scrim = animateColor(scrim),
+    surfaceBright = animateColor(surfaceBright),
+    surfaceDim = animateColor(surfaceDim),
+    surfaceContainer = animateColor(surfaceContainer),
+    surfaceContainerHigh = animateColor(surfaceContainerHigh),
+    surfaceContainerHighest = animateColor(surfaceContainerHighest),
+    surfaceContainerLow = animateColor(surfaceContainerLow),
+    surfaceContainerLowest = animateColor(surfaceContainerLowest),
+)
